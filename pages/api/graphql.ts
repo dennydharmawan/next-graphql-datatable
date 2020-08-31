c178ca10be3680ad24f08bc7d884a37c57a63778
denny dharmawan <denny.dharmawan@btpn.com>
2020-08-31 20:02:53 +0700

    initial commit

--- pages/api/graphql.ts
+++ pages/api/graphql.ts
@@ -1,6 +1,10 @@
-import app, { server } from 'nexus';
-import '../../graphql/schema';
+import "../../graphql/schema";
+
+import app, { server } from "nexus";
 
 app.assemble();
 
 export default server.handlers.graphql;
+
+//https://developer.mongodb.com/how-to/nextjs-building-modern-applications/
+//https://hoangvvo.com/blog/full-fledged-app-with-next-js-and-mongodb-part-1/
